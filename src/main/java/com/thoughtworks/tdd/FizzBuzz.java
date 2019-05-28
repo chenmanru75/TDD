660224823de8a33bc9d5ceb13b7ba5a624ac6e18
chenmanru75 <chenmanru75@163.com>
2019-05-28 15:50:21 +0800

    test ok

---
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.tdd;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FizzBuzz {
+    final private Map<Integer, String> fizzMap;
+
+    public FizzBuzz() {
+        this.fizzMap = new HashMap<>();
+        this.fizzMap.put(3, "Fizz");
+        this.fizzMap.put(5, "Buzz");
+        this.fizzMap.put(7, "Whizz");
+    }
+
+    public String fizzBuzz(int i) {
+        final String[] result = {""};
+        this.fizzMap.forEach((k, v) -> {
+            if (i % k == 0) {
+                result[0] += v;
+            }
+        });
+        if (result[0].equals("")) {
+            return String.valueOf(i);
+        }
+        return result[0];
+    }
+}
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -1,4 +1,69 @@
 package com.thoughtworks.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void should_return_1_given_FizzBuzz_when_fizz_buzz_input_1() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(1);
+        //then
+        assertEquals("1", actual);
+    }
+
+    @Test
+    void should_return_2_given_FizzBuzz_when_fizz_buzz_input_2() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(2);
+        //then
+        assertEquals("2", actual);
+    }
+    @Test
+    void should_return_Fizz_given_FizzBuzz_when_fizz_buzz_input_3() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(3);
+        //then
+        assertEquals("Fizz", actual);
+    }
+
+    @Test
+    void should_return_Buzz_given_FizzBuzz_when_fizz_buzz_input_5() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(5);
+        //then
+        assertEquals("Buzz", actual);
+    }
+
+    @Test
+    void should_return_Whizz_given_FizzBuzz_when_fizz_buzz_input_7() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(7);
+        //then
+        assertEquals("Whizz", actual);
+    }
+
+    @Test
+    void should_return_FizzBuzz_given_FizzBuzz_when_fizz_buzz_input_15() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(15);
+        //then
+        assertEquals("FizzBuzz", actual);
+    }
+    @Test
+    void should_return_FizzWhizz_given_FizzBuzz_when_fizz_buzz_input_21() {
+        //when
+        String actual = fizzBuzz.fizzBuzz(21);
+        //then
+        assertEquals("FizzWhizz", actual);
+    }
 }
